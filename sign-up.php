4bdb31d9913445fe3d9ad9d471f7cdf086a9d28d
Ashish Labade (Vegan) <ashishvegan@gmail.com>
2024-02-09 21:04:27 +0530

    09-Feb-2024

--- sign-up.php
+++ sign-up.php
@@ -1,3 +1,28 @@
+<?php
+require_once("config.php");
+if (!empty($_POST)) {
+    // Save Form Data in the PHP Variables
+    $fname = $_POST['fullName'];
+    $em = $_POST['email'];
+    $gen = $_POST['gender'];
+    $dob = $_POST['dob'];
+    $add = $_POST['add'];
+    $pass = sha1($_POST['pass']);
+    //echo "$fname $em $gen $dob $add $pass"; //Test Data
+    $newDate = date("d-m-Y", strtotime($dob));
+    $time = date('h:i a');
+    $date = date('d-m-Y');
+    $hash = uniqid();
+
+    // Insert Operation
+    $sql = mysqli_query($auth, "INSERT INTO students(hash_code,full_name,email,gender,dob,address,password,time,date) VALUES('$hash','$fname','$em','$gen','$dob','$add','$pass','$time','$date')");
+    if ($sql) {
+        $status = "1";
+    } else {
+        $status = "0";
+    }
+}
+?>
 <!doctype html>
 <html lang="en">
 
@@ -16,9 +41,23 @@
     ?>
     <div class="container">
         <h1 class="display-4 border-bottom">Student Registration Portal</h1>
+
+        <?php
+        if ($status == "1") { ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>Success!</strong> Registration completed.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php } elseif ($status == "0") { ?>
+
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Error!</strong> Account already exist.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php }
+        ?>
         <form method="post" action="">
             <div class="row row-cols-md-2 row-cols-1 py-4">
-
                 <div class="col">
                     <div class="form-floating mb-3">
                         <input type="text" class="form-control" id="fullName" name="fullName" placeholder="Enter Full Name" required>
@@ -28,7 +67,7 @@
 
                 <div class="col">
                     <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="email" name="email" placeholder="Enter Email" required> 
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Enter Email" required>
                         <label for="email">Email</label>
                     </div>
                 </div>
@@ -68,7 +107,7 @@
                         <label for="pass">Password</label>
                     </div>
                 </div>
-                
+
                 <div class="col">
                     <input type="submit" class="btn btn-dark btn-lg" value="Sign Up">
                 </div>
