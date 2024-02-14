4384036abffae4d6e9920c994c7d3aa660420e13
Ashish Labade (Vegan) <ashishvegan@gmail.com>
2024-02-14 21:02:19 +0530

    14-Feb-2024

--- academic-details.php
+++ academic-details.php
@@ -8,6 +8,40 @@ if (!isset($_SESSION['email'])) {
 $email = $_SESSION['email'];
 $sql = mysqli_query($auth, "SELECT * FROM students WHERE email = '$email'");
 $printData = mysqli_fetch_array($sql);
+
+// Insert Academic Details
+if(!empty($_POST))
+{
+    $roll = $_POST['roll'];
+    $br = $_POST['branch'];
+    $sem = $_POST['sem'];
+    $sec = $_POST['section'];
+    $sports = $_POST['sports'];
+    $last_update = date('h:i a | d-m-Y');
+
+    // File Upload
+    $file_name = $_FILES["file"]["name"];
+    $ext = end(explode(".",$file_name));
+    $file_name = uniqid().".".$ext;
+
+    move_uploaded_file($_FILES["file"]["tmp_name"],"pictures/".$file_name);
+    //echo $file_name;
+    //echo " Location:".$_FILES["file"]["tmp_name"];
+
+    // Insert Records
+    $qr = mysqli_query($auth,"INSERT INTO academic_details(email,roll_no,branch,sem,section,sports,profile_photo,last_update) VALUES('$email','$roll','$br','$sem','$sec','$sports','$file_name','$last_update')");
+    if($qr)
+    {
+        $status = "1";
+    }
+    else
+    {
+        $status = "0";
+    }
+}
+// Read Saved Information
+$rsi = mysqli_query($auth, "SELECT * FROM academic_details WHERE email = '$email'");
+$prD = mysqli_fetch_array($rsi);
 ?>
 <!doctype html>
 <html lang="en">
@@ -16,7 +50,8 @@ $printData = mysqli_fetch_array($sql);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Academic Details | Student Information System</title>
 </head>
@@ -27,11 +62,28 @@ $printData = mysqli_fetch_array($sql);
     ?>
     <div class="container">
         <h1 class="display-4 border-bottom">Academic Details</h1>
+
+        <?php
+        if ($status == "1") { ?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Success!</strong> Your academic details have been saved.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        <?php } elseif ($status == "0") { ?>
+
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> Please try later.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        <?php }
+        ?>
+
         <form method="post" action="" enctype="multipart/form-data">
             <div class="row py-3 row-cols-md-1 row-cols-1">
                 <div class="col mb-3">
                     <label class="form-label" for="roll">Roll No.</label>
-                    <input type="text" name="roll" id="roll" class="form-control form-control-lg" required placeholder="Enter Roll No.">
+                    <input type="text" name="roll" id="roll" class="form-control form-control-lg" required
+                        placeholder="Enter Roll No." value="<?php echo $prD['roll_no'];?>">
                 </div>
                 <div class="col mb-3">
                     <select name="branch" class="form-select form-select-lg" required>
@@ -39,7 +91,8 @@ $printData = mysqli_fetch_array($sql);
                         <option value="Computer Science and Engineering">Computer Science and Engineering</option>
                         <option value="Mechanical Engineering">Mechanical Engineering</option>
                         <option value="Electrical Engineering">Electrical Engineering</option>
-                        <option value="Electronics and Communication Engineering">Electronics and Communication Engineering</option>
+                        <option value="Electronics and Communication Engineering">Electronics and Communication
+                            Engineering</option>
                     </select>
                 </div>
                 <div class="col mb-3">
@@ -49,7 +102,7 @@ $printData = mysqli_fetch_array($sql);
                         <?php
                         for ($x = 1; $x <= 8; $x++) {
                         ?>
-                            <option value="<?php echo $x; ?>"><?php echo $x; ?></option>
+                        <option value="<?php echo $x; ?>"><?php echo $x; ?></option>
                         <?php
                         }
                         ?>
@@ -67,12 +120,13 @@ $printData = mysqli_fetch_array($sql);
                 </div>
                 <div class="col mb-3">
                     <label for="sports" class="form-label">Sports</label>
-                    <input type="text" name="sports" class="form-control form-control-lg" id="sports" required placeholder="Enter Sports">
+                    <input type="text" name="sports" class="form-control form-control-lg" id="sports" required
+                        placeholder="Enter Sports" value="<?php echo $prD['sports'];?>">
                 </div>
                 <div class="col mb-3">
                     <div class="input-group mb-3">
                         <label class="input-group-text" for="file">Upload</label>
-                        <input type="file" class="form-control" id="file" name="file">
+                        <input type="file" class="form-control" id="file" name="file" accept="image/png, image/jpeg">
                     </div>
                 </div>
                 <div class="col mb-3">
@@ -80,7 +134,9 @@ $printData = mysqli_fetch_array($sql);
                 </div>
             </div>
         </form>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+        </script>
 </body>
 
 </html>
